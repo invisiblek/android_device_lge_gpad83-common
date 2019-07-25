744dd346d7a8796f548570bc325331118fa9a19e
Dan Pasanen <dan.pasanen@gmail.com>
2019-07-25 11:09:11 -0500

    gpad83-common: update display packages

--- gpad83-common.mk
+++ gpad83-common.mk
@@ -111,13 +111,11 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.graphics.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    libgenlock \
-    hwcomposer.msm8960 \
-    gralloc.msm8960 \
     copybit.msm8960 \
+    gralloc.msm8960 \
+    hwcomposer.msm8960 \
     memtrack.msm8960
 
 PRODUCT_PROPERTY_OVERRIDES += \
