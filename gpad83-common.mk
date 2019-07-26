fce252ad3fca004814c918266f935b2c848ef17a
Dan Pasanen <dan.pasanen@gmail.com>
2019-07-26 12:31:04 -0500

    gpad83-common: sync media_codecs_performance.xml to beroid's tree

--- gpad83-common.mk
+++ gpad83-common.mk
@@ -169,6 +169,7 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/vendor/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs_google_performance.xml:system/vendor/etc/media_codecs_google_performance.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/vendor/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:system/vendor/etc/media_profiles_V1_0.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/vendor/etc/media_codecs_google_audio.xml \
