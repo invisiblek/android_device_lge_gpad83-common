aee91c5b8e6a0868373985980fe636f136d30896
Dan Pasanen <dan.pasanen@gmail.com>
2019-07-29 14:11:23 -0500

    gpad83-common: add init.recovery.qcom.rc
    * For setting sys.usb.ffs.aio_compat prop and creating the symlink
      at /dev/block/bootdevice in recovery

--- gpad83-common.mk
+++ gpad83-common.mk
@@ -222,6 +222,7 @@ PRODUCT_PACKAGES += \
 # Rootdir
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
+    $(LOCAL_PATH)/rootdir/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.power.rc:root/init.qcom.power.rc \
     $(LOCAL_PATH)/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
