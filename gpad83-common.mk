84e09aa08f575c94bd2efbb44f62fbde357fa942
Bruno Martins <bgcngm@gmail.com>
2019-10-04 23:20:42 +0100

    gpad83-common: Switch to legacy Wi-Fi service
    Change-Id: I690bc80349e47907a29a89b8a6f96659f1c61aba

--- gpad83-common.mk
+++ gpad83-common.mk
@@ -257,7 +257,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     conn_init \
     hostapd \
     wificond \
